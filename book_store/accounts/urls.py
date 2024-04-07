8201aec2d41ed70247cc95b14d3b37e2aba544f5
MohamedAbotalb <mohamed.abotalb277@gmail.com>
2024-04-07 14:09:45 +0200

    Add user registration functionality

--- book_store/accounts/urls.py
+++ book_store/accounts/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('profile/', views.profile_view, name='profile_view'),
+    path('register/', views.register_user, name='register_user'),
 ]
--- book_store/accounts/views.py
+++ book_store/accounts/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render, redirect, reverse
+from accounts.forms import RegistrationForm
+
 
 # Create your views here.
 
@@ -6,3 +8,15 @@ from django.shortcuts import render, redirect, reverse
 def profile_view(request):
     url = reverse('index_page')
     return redirect(url)
+
+
+def register_user(request):
+    form = RegistrationForm()
+    if request.method == "POST":
+        form = RegistrationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login_url = reverse('login')
+            return redirect(login_url)
+
+    return render(request, 'accounts/register.html', {'form': form})
